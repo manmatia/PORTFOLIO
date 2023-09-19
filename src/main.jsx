640afa737b61985632e90931cfe06dccb52084cf
Matias Graner <matiasgraner@MacBook-Pro-de-Matias.local>
2023-09-19 13:50:30 -0300

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import App from './App.jsx'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
